1a13da0d5ada7b2d181010beff37b37d8c15e019
gramsower <110431307+gramsower@users.noreply.github.com>
2022-10-03 19:12:23 -0700

    finalize template

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'template',
       template: './src/index.html',
       inject: 'body'
     })
